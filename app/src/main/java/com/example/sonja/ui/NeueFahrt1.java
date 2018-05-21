d6e1c48ca4f2a9fb758cf7a0d577c289fd2dabca
sonni93 <38693058+sonni93@users.noreply.github.com>
2018-05-21 11:29:38 +0200

    Fahrten1: Time Picker

--- app/src/main/java/com/example/sonja/ui/NeueFahrt1.java
+++ app/src/main/java/com/example/sonja/ui/NeueFahrt1.java
@@ -1,5 +1,6 @@
 package com.example.sonja.ui;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class NeueFahrt1 extends AppCompatActivity implements View.OnClickListener{
 
@@ -16,6 +21,8 @@ public class NeueFahrt1 extends AppCompatActivity implements View.OnClickListene
     Button btn_search;
     Button btn_offer;
     Button btn_both;
+    EditText txt_abfahrtszeit;
+    EditText txt_ankunftszeit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,12 @@ public class NeueFahrt1 extends AppCompatActivity implements View.OnClickListene
 
         btn_both = findViewById(R.id.btn_both);
         btn_both.setOnClickListener(this);
+
+        txt_abfahrtszeit = findViewById(R.id.txt_abfahrtszeit);
+        txt_abfahrtszeit.setOnClickListener(this);
+
+        txt_ankunftszeit = findViewById(R.id.txt_ankunftszeit);
+        txt_ankunftszeit.setOnClickListener(this);
 
         //get the spinner from the xml.
         Spinner dropdown = findViewById(R.id.spinner_anzahl_sitze);
@@ -80,6 +93,40 @@ public class NeueFahrt1 extends AppCompatActivity implements View.OnClickListene
                 btn_search.setBackgroundResource(R.drawable.button_style);
                 btn_offer.setBackgroundResource(R.drawable.button_style);
                 btn_both.setBackgroundResource(R.drawable.button_style_clicked);
+                break;
+
+            case R.id.txt_abfahrtszeit:
+                // Abfahrtszeit Time Picker dialogue
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        txt_abfahrtszeit.setText( selectedHour + ":" + selectedMinute + "  Uhr");
+                    }
+                }, hour, minute, true);
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+                break;
+
+            case R.id.txt_ankunftszeit:
+                // Ankunftszeit Time Picker dialogue
+                Calendar ncurrentTime = Calendar.getInstance();
+                int nhour = ncurrentTime.get(Calendar.HOUR_OF_DAY);
+                int nminute = ncurrentTime.get(Calendar.MINUTE);
+
+                TimePickerDialog nTimePicker;
+                nTimePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker ntimePicker, int nselectedHour, int nselectedMinute) {
+                        txt_ankunftszeit.setText( nselectedHour + ":" + nselectedMinute + "  Uhr");
+                    }
+                }, nhour, nminute, true);
+                nTimePicker.setTitle("Select Time");
+                nTimePicker.show();
                 break;
 
             default:
